fd87694a15497d92cdf56ec80522dcf31f523852
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2021-01-09 22:04:14 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var paypal;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AppPaypal';
+export class AppComponent implements OnInit{
+
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
+
+  producto = {
+    descripcion : 'producto en venta',
+    precio      : 599.99,
+    img         : 'imagen de tu producto'
+  };
+  title = 'angular-paypal-payment';
+
+  // tslint:disable-next-line:typedef
+  ngOnInit(){
+    paypal.Buttons({
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: this.producto.descripcion,
+              amount: {
+                currency_code: 'USD',
+                value        : this.producto.precio
+              }
+            }
+          ]
+        });
+      },
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log(order);
+      },
+      onError: err => {
+        console.log(err);
+      }
+    })
+    .render(this.paypalElement.nativeElement);
+  }
 }
